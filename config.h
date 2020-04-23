464093ffe8756240fcad0c15e6938c0dfcee1e3d
Ashish Panigrahi <panigrahi.ashish1999@gmail.com>
2020-04-23 16:01:08 +0000

    Added awesome and material icon fonts

--- config.h
+++ config.h
@@ -14,7 +14,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=12" };
+static const char *fonts[]          = { "JetBrains Mono:size=12",
+					"FontAwesome:style=Regular:size=11:antialias=true:autohint=true",
+					"MaterialIcons:style=Regular:size=13:antialias=true:autohint=true"
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
