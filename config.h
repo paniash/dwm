5515050f3a483106ce9905926fed3145f5a9af5c
Ashish Panigrahi <panigrahi.ashish1999@gmail.com>
2020-05-16 14:29:21 +0000

    Changed statusbar fonts-> IBM Plex Mono; Changed bar colour

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=12",
+static const char *fonts[]          = { "IBM Plex Mono:size=12",
 					"IPAGothic:style=Regular",
 					"FontAwesome:style=Regular:size=11:antialias=true:autohint=true",
 					"MaterialIcons:style=Regular:size=13:antialias=true:autohint=true"
@@ -28,12 +28,13 @@ static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, NULL },
-	[SchemeSel]  = { col_gray4, "#e0612a",  "#9f9ea8"  },
+	[SchemeSel]  = { col_gray4, "#477AB3",  "#9f9ea8"  },
 };
 
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
-static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+/* static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" }; */
+static const char *tags[] = { "一", "二", "三", "四", "五", "六" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -41,8 +42,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 7,            1,           -1 },
-	{ "Slack",     NULL,       NULL,       1 << 6,            0,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 5,            1,           -1 },
+	{ "Slack",     NULL,       NULL,       1 << 4,            0,           -1 },
 };
 
 /* layout(s) */
