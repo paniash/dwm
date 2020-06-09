18e3f7936c61f3dca91f46819b819879ff5ffddb
Ashish Panigrahi <panigrahi.ashish1999@gmail.com>
2020-06-09 22:54:34 +0000

    Modified resetlayouts to resetmfact patch

--- dwm.c
+++ dwm.c
@@ -1566,10 +1566,7 @@ removesystrayicon(Client *i)
 void
 resetlayout(const Arg *arg)
 {
-		Arg default_layout = {.v = &layouts[0]};
 		Arg default_mfact = {.f = mfact + 1};
-
-		setlayout(&default_layout);
 		setmfact(&default_mfact);
 }
 
