2a54a066d39f2e2c8f14d80b465d185eb146a6b9
Ashish Panigrahi <panigrahi.ashish1999@gmail.com>
2020-06-20 17:39:37 +0000

    Changed font to JetBrains mono

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "IBM Plex Mono:size=12",
+static const char *fonts[]          = { "JetBrains Mono:size=12",
 					"IPAGothic:style=Regular",
 					"FontAwesome:style=Regular:size=11:antialias=true:autohint=true",
 					"MaterialIcons:style=Regular:size=13:antialias=true:autohint=true"
