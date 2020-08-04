b7ca0a61b98165261b7b0c843f9ae82451ce6001
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2020-08-04 12:56:31 +0530

    make Gimp unfloating

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",           NULL,      NULL,       1 << 4,       1,           -1 },
+	{ "Gimp",           NULL,      NULL,       1 << 4,       0,           -1 },
 	{ "Matplotlib",     NULL,      NULL,       0,            1,           -1 },
 	{ "R_x11",          NULL,      NULL,       0,            1,           -1 },
 	{ "Qalculate-gtk",  NULL,      NULL,       0,            1,           -1 },
