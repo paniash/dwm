7377dbfd3f650c232a76a9671d123901ba6eb3e4
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-01-15 23:35:13 +0530

    added window rules for discord and slack

--- config.h
+++ config.h
@@ -63,6 +63,8 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           1 << 4,         0,          0,           1,        -1 },
+	{ "discord",    NULL,     NULL,           1 << 8,         0,          0,           1,        -1 },
+	{ "Slack",    NULL,     NULL,           1 << 7,         0,          0,           1,        -1 },
 	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Matplotlib",     NULL,      NULL,       0,            1, 0, 1,           -1 },
 	{ "R_x11",          NULL,      NULL,       0,            1, 0, 1,           -1 },
