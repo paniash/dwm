b7aba48a6fe09f6933c6683e50af7ec02c04e7df
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-06-05 22:56:10 +0530

    commented out third layout

--- config.h
+++ config.h
@@ -154,7 +154,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,					    XK_e,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
+	/* { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} }, */
 	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
 	{ MODKEY|ShiftMask,             XK_e,      togglefloating, {0} },
 	{ MODKEY,                       XK_f,      togglefullscreen,  {0} },
